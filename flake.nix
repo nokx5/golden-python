4e2553caa42e23a1e520546cec8c2c9db9ae7567
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-14 15:48:27 +0200

    flake works perfectly !

--- flake.nix
+++ flake.nix
@@ -45,30 +45,17 @@
                       nativeBuildInputs = let
                         vscodeExt = vscode-with-extensions.override {
                           vscodeExtensions = with vscode-extensions;
-                            [ bbenoist.Nix ms-vscode.cpptools ]
-                            ++ vscode-utils.extensionsFromVscodeMarketplace [
-                              {
-                                name = "cmake-tools";
-                                publisher = "ms-vscode";
-                                version = "1.7.3";
-                                sha256 =
-                                  "6UJSJETKHTx1YOvDugQO194m60Rv3UWDS8UXW6aXOko=";
-                              }
-                              {
-                                name = "emacs-mcx";
-                                publisher = "tuttieee";
-                                version = "0.31.0";
-                                sha256 =
-                                  "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";
-                              }
-                              {
-                                name = "cmake";
-                                publisher = "twxs";
-                                version = "0.0.17";
-                                sha256 =
-                                  "CFiva1AO/oHpszbpd7lLtDzbv1Yi55yQOQPP/kCTH4Y=";
-                              }
-                            ];
+                            [
+                              bbenoist.Nix
+                              ms-python.python
+                              ms-python.vscode-pylance
+                            ] ++ vscode-utils.extensionsFromVscodeMarketplace [{
+                              name = "emacs-mcx";
+                              publisher = "tuttieee";
+                              version = "0.31.0";
+                              sha256 =
+                                "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";
+                            }];
                         };
                       in [ cacert hugo emacs-nox nixfmt typora vscodeExt ]
                       ++ oldAttrs.nativeBuildInputs;
