7118e816ec19955d66898928e849847b981ca010
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-14 15:38:40 +0200

    Update flake.nix with devShell!

--- flake.nix
+++ flake.nix
@@ -14,14 +14,15 @@
             pythonPackageOverrides = python-self: python-super:
               let inherit (python-super) buildPythonPackage lib pythonPackages;
               in with python-self; {
-                # golden_python = buildPythonPackage rec {
-                #           pname = "golden_python";
-                #           version = "0.0";
-                #           src = self;
-                # 	    checkInputs = [ pytestCheckHook ];
-                #           pytestFlagsArray = [ "tests" "-vv" ];
-                #           propagatedBuildInputs = [ decorator jinja2 pyjson5 toml ];
-                # };
+
+                golden_python = buildPythonPackage rec {
+                  pname = "golden_python";
+                  version = "0.0";
+                  src = self;
+                  checkInputs = [ pytestCheckHook ];
+                  pytestFlagsArray = [ "tests" "-vv" ];
+                  propagatedBuildInputs = [ decorator jinja2 pyjson5 toml ];
+                };
 
                 cli_golden_python = pythonPackages.buildPythonApplication rec {
                   pname = "cli_golden_python";
@@ -37,24 +38,64 @@
                   #   touch $outputDoc/share/doc/dflkfld/helloworld
                   # '';
                 };
+
+                dev = (with pkgs-self;
+                  (with python-self;
+                    golden_python.overrideAttrs (oldAttrs: rec {
+                      nativeBuildInputs = let
+                        vscodeExt = vscode-with-extensions.override {
+                          vscodeExtensions = with vscode-extensions;
+                            [ bbenoist.Nix ms-vscode.cpptools ]
+                            ++ vscode-utils.extensionsFromVscodeMarketplace [
+                              {
+                                name = "cmake-tools";
+                                publisher = "ms-vscode";
+                                version = "1.7.3";
+                                sha256 =
+                                  "6UJSJETKHTx1YOvDugQO194m60Rv3UWDS8UXW6aXOko=";
+                              }
+                              {
+                                name = "emacs-mcx";
+                                publisher = "tuttieee";
+                                version = "0.31.0";
+                                sha256 =
+                                  "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";
+                              }
+                              {
+                                name = "cmake";
+                                publisher = "twxs";
+                                version = "0.0.17";
+                                sha256 =
+                                  "CFiva1AO/oHpszbpd7lLtDzbv1Yi55yQOQPP/kCTH4Y=";
+                              }
+                            ];
+                        };
+                      in [ cacert hugo emacs-nox nixfmt typora vscodeExt ]
+                      ++ oldAttrs.nativeBuildInputs;
+                      shellHook = ''
+                        export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+                        export PYTHONPATH=$PWD:$PYTHONPATH
+                      '';
+                    })));
+
               };
 
-          in (with pkgs-self; {
+          in {
 
             python37 = pkgs-super.python37.override (old: {
-              packageOverrides = pkgs-super.stdenv.lib.composeExtensions
+              packageOverrides = pkgs-super.lib.composeExtensions
                 (old.packageOverrides or (_: _: { })) pythonPackageOverrides;
             });
             python38 = pkgs-super.python38.override (old: {
-              packageOverrides = pkgs-super.stdenv.lib.composeExtensions
+              packageOverrides = pkgs-super.lib.composeExtensions
                 (old.packageOverrides or (_: _: { })) pythonPackageOverrides;
             });
             python39 = pkgs-super.python39.override (old: {
-              packageOverrides = pkgs-super.stdenv.lib.composeExtensions
+              packageOverrides = pkgs-super.lib.composeExtensions
                 (old.packageOverrides or (_: _: { })) pythonPackageOverrides;
             });
-            python3 = python38;
-          });
+            python3 = pkgs-self.python38;
+          };
 
         pkgs = import nixpkgs {
           inherit system;
@@ -62,59 +103,12 @@
           overlays = [ overlay ];
         };
 
-        # dev = (with pkgs;
-        #   golden_python.overrideAttrs (oldAttrs: rec {
-        #     nativeBuildInputs = let
-        #       vscodeExt = vscode-with-extensions.override {
-        #         vscodeExtensions = with vscode-extensions;
-        #           [ bbenoist.Nix ms-vscode.cpptools ]
-        #           ++ vscode-utils.extensionsFromVscodeMarketplace [
-        #             {
-        #               name = "cmake-tools";
-        #               publisher = "ms-vscode";
-        #               version = "1.7.3";
-        #               sha256 = "6UJSJETKHTx1YOvDugQO194m60Rv3UWDS8UXW6aXOko=";
-        #             }
-        #             {
-        #               name = "emacs-mcx";
-        #               publisher = "tuttieee";
-        #               version = "0.31.0";
-        #               sha256 = "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";
-        #             }
-        #             {
-        #               name = "cmake";
-        #               publisher = "twxs";
-        #               version = "0.0.17";
-        #               sha256 = "CFiva1AO/oHpszbpd7lLtDzbv1Yi55yQOQPP/kCTH4Y=";
-        #             }
-        #           ];
-        #       };
-        #     in [
-        #       # stdenv.cc.cc
-        #       # libcxxabi	      
-        #       bashCompletion
-        #       cacert
-        #       clang-tools
-        #       cmake-format
-        #       cmakeCurses
-        #       gdb
-        #       pkg-config
-        #       hugo
-        #       emacs-nox
-        #       nixfmt
-        #       vscodeExt
-        #       typora
-        #     ] ++ oldAttrs.nativeBuildInputs;
-        #     shellHook = ''
-        #       export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
-        #     '';
-        #   }));
-        #	golden_python = pkgs.python3Packages.golden_python;
-        cli_golden_python = pkgs.python3Packages.cli_golden_python;
-
       in {
-        packages = { inherit cli_golden_python; };
+        packages = {
+          cli_golden_python = pkgs.python3Packages.cli_golden_python;
+          golden_python = pkgs.python3Packages.golden_python;
+        };
         defaultPackage = self.packages.${system}.cli_golden_python;
-        # devShell = dev;
+        devShell = pkgs.python3Packages.dev;
       });
 }
